5af85a8acd15cbfaf33fd58e45649683f90f59b0
rtrevill <139879271+rtrevill@users.noreply.github.com>
2024-01-26 10:56:19 +1100

    working on queries and mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,6 +4,26 @@ const resolvers = {
   Query: {
     users: async () => {
       return await User.find()
+    },
+
+    user: async (_,{userId}) => {
+      return await User.findOne({_id: userId})
+    },
+  },
+
+  Mutation: {
+    createUser: async(_, {username, email, password}) => {
+      const newUser = await User.create({username, email, password});
+      return newUser;
+    },
+
+    saveBook: async(_,{_id, title}) => {
+      const newBook = await User.findOneAndUpdate(
+        {_id: _id},
+        { $addToSet: { savedBooks: {title: title} } },
+        { new: true, runValidators: true }
+      );
+      return newBook
     }
   }
 };
