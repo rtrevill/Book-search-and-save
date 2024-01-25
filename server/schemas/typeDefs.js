d0bf955ae2597446f9c371e09dc1b3e8bca51b3b
rtrevill <139879271+rtrevill@users.noreply.github.com>
2024-01-26 09:30:10 +1100

    ensured typeDefs are correct

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,18 @@
 const typeDefs = `
   type User {
-    _id: ID
     username: String
     email: String
     password: String
+    savedBooks: [Book]
+  }
+
+  type Book {
+    authors: [String]!
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
   }
 
   type Query {
